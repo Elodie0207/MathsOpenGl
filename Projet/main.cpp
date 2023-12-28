ea5d19292bc875e876b94b1868922306c39b154e
Elodie0207 <elodieguldas@gmail.com>
2023-12-28 16:44:43 +0100

    Adding Glut

--- Projet/main.cpp
+++ Projet/main.cpp
@@ -1,50 +1,31 @@
 #include <iostream>
-#include <GL/glew.h>
+
 #include <GLFW/glfw3.h>
+#include <GL/freeglut.h>
 
-int main() {
-    // Initialisation de GLFW
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
 
-    // Configuration GLFW pour utiliser OpenGL Core Profile 3.3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT); // Clear the color buffer
 
-    // Création d'une fenêtre GLFW
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Fenêtre", nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+    // Draw something here
+    // Example: Draw a triangle
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0, 0.0, 0.0); // Set color to red
+    glVertex2f(-0.5, -0.5);
+    glColor3f(0.0, 1.0, 0.0); // Set color to green
+    glVertex2f(0.5, -0.5);
+    glColor3f(0.0, 0.0, 1.0); // Set color to blue
+    glVertex2f(0.0, 0.5);
+    glEnd();
 
-    // Activation du contexte de la fenêtre
-    glfwMakeContextCurrent(window);
+    glFlush(); // Flush the rendering pipeline
+}
 
-    // Initialisation de GLEW (après avoir créé le contexte OpenGL)
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Failed to initialize GLEW" << std::endl;
-        return -1;
-    }
+int main(int argc, char** argv) {
+    glutInit(&argc, argv); // Initialize GLUT
+    glutCreateWindow("Simple Window"); // Create a window with the given title
+    glutDisplayFunc(display); // Set the display callback function
+    glutMainLoop(); // Enter the GLUT event processing loop
 
-    // Boucle principale
-    while (!glfwWindowShouldClose(window)) {
-        // Vérification des événements
-        glfwPollEvents();
-
-        // Effacement du buffer
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // Swap des buffers
-        glfwSwapBuffers(window);
-    }
-
-    // Fermeture de la fenêtre
-    glfwDestroyWindow(window);
-    glfwTerminate();
     return 0;
 }
