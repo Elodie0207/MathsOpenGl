ebdde9f72f4036372e92d9fb828c922b2171c378
Iannis Poquet <ianpo@live.fr>
2023-12-29 15:33:11 +0100

    Add iostream overload for Math

--- Projet/include/Math.hpp
+++ Projet/include/Math.hpp
@@ -16,15 +16,55 @@
 #include <glm/gtx/hash.hpp>
 #include <cmath>
 #include <cstdint>
+#include <iostream>
 
 typedef glm::vec2 Vec2;
 typedef glm::ivec2 Vec2Int;
+typedef glm::vec3 Vec3;
+typedef glm::vec4 Vec4;
+typedef glm::quat Quat;
+
+typedef glm::mat4 Mat4;
 
 class Math
 {
+public:
     template<typename T>
-    std::string to_string(const T& value)
+    inline static std::string ToString(const T& value)
     {
         return glm::to_string(value);
     }
+
+	template<typename T>
+	inline static T Normalize(const T& val)
+	{
+		return glm::normalize(val);
+	}
+
+	static Mat4 TRS(Vec3 position);
+	static Mat4 TRS(Vec3 position, Quat rotation);
+	static Mat4 TRS(Vec3 position, Vec3 scale);
+	static Mat4 TRS(Vec3 position, Quat Rotation, Vec3 scale);
+
+	static Mat4 Inverse(Mat4 mat);
+
 };
+
+inline std::ostream& operator << (std::ostream & os, const Mat4& value){
+	return os << Math::ToString(value);
+}
+inline std::ostream& operator << (std::ostream & os, const Vec2Int& value){
+	return os << Math::ToString(value);
+}
+inline std::ostream& operator << (std::ostream & os, const Vec4& value){
+	return os << Math::ToString(value);
+}
+inline std::ostream& operator << (std::ostream & os, const Vec3& value){
+	return os << Math::ToString(value);
+}
+inline std::ostream& operator << (std::ostream & os, const Vec2& value){
+	return os << Math::ToString(value);
+}
+inline std::ostream& operator << (std::ostream & os, const Quat& value){
+	return os << Math::ToString(value);
+}
