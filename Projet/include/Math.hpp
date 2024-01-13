ae96e4d5722269221a9b8acd4a1478182dbdf383
Iannis Poquet <ianpo@live.fr>
2024-01-13 23:49:17 +0100

    Use tab in Math.hpp

--- Projet/include/Math.hpp
+++ Projet/include/Math.hpp
@@ -38,60 +38,60 @@ typedef glm::dvec2 Point;
 class Math
 {
 public:
-    template<typename T>
-    inline static std::string ToString(const T& value)
-    {
-        return glm::to_string(value);
-    }
+	template<typename T>
+	inline static std::string ToString(const T& value)
+	{
+		return glm::to_string(value);
+	}
 
-    template<typename T>
-    inline static T Normalize(const T& val)
-    {
-        return glm::normalize(val);
-    }
+	template<typename T>
+	inline static T Normalize(const T& val)
+	{
+		return glm::normalize(val);
+	}
 
-    template<typename T>
-    inline static float Distance(const T& l, const T& r)
-    {
-        return glm::distance(l, r);
-    }
+	template<typename T>
+	inline static float Distance(const T& l, const T& r)
+	{
+		return glm::distance(l, r);
+	}
 
-    template<typename T>
-    inline static T Min(const T& v1, const T& v2)
-    {
-        return glm::min(v1, v2);
-    }
+	template<typename T>
+	inline static T Min(const T& v1, const T& v2)
+	{
+		return glm::min(v1, v2);
+	}
 
-    template<typename T>
-    inline static T Max(const T& v1, const T& v2)
-    {
-        return glm::max(v1, v2);
-    }
+	template<typename T>
+	inline static T Max(const T& v1, const T& v2)
+	{
+		return glm::max(v1, v2);
+	}
 
-    static Mat4 TRS(Vec3 position);
-    static Mat4 TRS(Vec3 position, Quat rotation);
-    static Mat4 TRS(Vec3 position, Vec3 scale);
-    static Mat4 TRS(Vec3 position, Quat Rotation, Vec3 scale);
-    static Mat4 TRS(Vec2 position);
-    static Mat4 TRS(Vec2 position, Vec2 scale);
+	static Mat4 TRS(Vec3 position);
+	static Mat4 TRS(Vec3 position, Quat rotation);
+	static Mat4 TRS(Vec3 position, Vec3 scale);
+	static Mat4 TRS(Vec3 position, Quat Rotation, Vec3 scale);
+	static Mat4 TRS(Vec2 position);
+	static Mat4 TRS(Vec2 position, Vec2 scale);
 
-    static Mat4 Inverse(Mat4 mat);
-    static bool CyrusBeck(double X1, double Y1, double X2, double Y2, std::vector<Point> Poly, std::vector<Point> Normale, int Nbsom);
-    static double coupe(const Point& a, const Point& b, const Point& c);
-    static Point intersection(const Point& a, const Point& b, const Point& c, const Point& d);
-    static std::vector<Point> sutherlandHodgman(std::vector<Point> PL, const std::vector<Point>& PW);
+	static Mat4 Inverse(Mat4 mat);
+	static bool CyrusBeck(double X1, double Y1, double X2, double Y2, std::vector<Point> Poly, std::vector<Point> Normale, int Nbsom);
+	static double coupe(const Point& a, const Point& b, const Point& c);
+	static Point intersection(const Point& a, const Point& b, const Point& c, const Point& d);
+	static std::vector<Point> sutherlandHodgman(std::vector<Point> PL, const std::vector<Point>& PW);
 
-    struct Color {
-        int r, g, b;
-    };
+	struct Color {
+		int r, g, b;
+	};
 
-    static void polygon_fill(const std::vector<Point>& poly, std::vector<std::vector<Color>>& image, const Color& color_fill);
-    static void fillRegionConnexity4(int x, int y, std::vector<std::vector<Color>>& image, const Color& colorContour, const Color& colorFill);
-
-
+	static void polygon_fill(const std::vector<Point>& poly, std::vector<std::vector<Color>>& image, const Color& color_fill);
+	static void fillRegionConnexity4(int x, int y, std::vector<std::vector<Color>>& image, const Color& colorContour, const Color& colorFill);
 
 
-    /**
+
+
+	/**
 	 * @brief Calculates the smallest integer value greater than or equal to the given value.
 	 *
 	 * This function returns the smallest integer value greater than or equal to the given value. It is primarily used to round up floating-point numbers to the nearest integer.
@@ -103,49 +103,49 @@ public:
 	 * @see <a href="http://www.opengl.org/sdk/docs/manglsl/xhtml/ceil.xml">GLSL ceil man page</a>
 	 * @see <a href="http://www.opengl.org/registry/doc/GLSLangSpec.4.20.8.pdf">GLSL 4.20.8 specification, section 8.3 Common Functions</a>
 	 */
-    template<typename T>
-    static T Ceil(T value)
-    {
-        return glm::ceil(value);
-    }
+	template<typename T>
+	static T Ceil(T value)
+	{
+		return glm::ceil(value);
+	}
 
 
-    /**
-     * @brief Calculates the nearest integer less than or equal to the given value.
-     *
-     * This function returns the nearest integer less than or equal to the given value. It is primarily used to round down floating-point numbers to the nearest integer.
-     *
-     * @tparam T The type of the input value.
-     * @param value The value to floor.
-     * @return The nearest integer less than or equal to the given value.
-     *
-     * @see <a href="http://www.opengl.org/sdk/docs/manglsl/xhtml/floor.xml">GLSL floor man page</a>
-     * @see <a href="http://www.opengl.org/registry/doc/GLSLangSpec.4.20.8.pdf">GLSL 4.20.8 specification, section 8.3 Common Functions</a>
-     */
-    template<typename T>
-    static T Floor(T value)
-    {
-        return glm::floor(value);
-    }
+	/**
+	 * @brief Calculates the nearest integer less than or equal to the given value.
+	 *
+	 * This function returns the nearest integer less than or equal to the given value. It is primarily used to round down floating-point numbers to the nearest integer.
+	 *
+	 * @tparam T The type of the input value.
+	 * @param value The value to floor.
+	 * @return The nearest integer less than or equal to the given value.
+	 *
+	 * @see <a href="http://www.opengl.org/sdk/docs/manglsl/xhtml/floor.xml">GLSL floor man page</a>
+	 * @see <a href="http://www.opengl.org/registry/doc/GLSLangSpec.4.20.8.pdf">GLSL 4.20.8 specification, section 8.3 Common Functions</a>
+	 */
+	template<typename T>
+	static T Floor(T value)
+	{
+		return glm::floor(value);
+	}
 
-    /**
-     * @brief Rounds a given value to the nearest integer.
-     *
-     * This function calculates the nearest integer to the given value. It rounds the value up or down based on its decimal component. If the decimal component is less than 0.5, the value
-    * is rounded down, otherwise it is rounded up.
-     *
-     * @tparam T The type of the value to be rounded.
-     * @param value The value to be rounded.
-     * @return The nearest integer value to the given input.
-     *
-     * @see <a href="http://www.opengl.org/sdk/docs/manglsl/xhtml/round.xml">GLSL round man page</a>
-     * @see <a href="http://www.opengl.org/registry/doc/GLSLangSpec.4.20.8.pdf">GLSL 4.20.8 specification, section 8.3 Common Functions</a>
-     */
-    template<typename T>
-    static T Round(T value)
-    {
-        return glm::round(value);
-    }
+	/**
+	 * @brief Rounds a given value to the nearest integer.
+	 *
+	 * This function calculates the nearest integer to the given value. It rounds the value up or down based on its decimal component. If the decimal component is less than 0.5, the value
+	* is rounded down, otherwise it is rounded up.
+	 *
+	 * @tparam T The type of the value to be rounded.
+	 * @param value The value to be rounded.
+	 * @return The nearest integer value to the given input.
+	 *
+	 * @see <a href="http://www.opengl.org/sdk/docs/manglsl/xhtml/round.xml">GLSL round man page</a>
+	 * @see <a href="http://www.opengl.org/registry/doc/GLSLangSpec.4.20.8.pdf">GLSL 4.20.8 specification, section 8.3 Common Functions</a>
+	 */
+	template<typename T>
+	static T Round(T value)
+	{
+		return glm::round(value);
+	}
 };
 
 template<> inline std::string Math::ToString(const float& value) { return std::to_string(value); }
@@ -164,20 +164,20 @@ template<> inline std::string Math::ToString(const signed long long int& value) 
 
 
 inline std::ostream& operator << (std::ostream & os, const Mat4& value){
-    return os << Math::ToString(value);
+	return os << Math::ToString(value);
 }
 inline std::ostream& operator << (std::ostream & os, const Vec2Int& value){
-    return os << Math::ToString(value);
+	return os << Math::ToString(value);
 }
 inline std::ostream& operator << (std::ostream & os, const Vec4& value){
-    return os << Math::ToString(value);
+	return os << Math::ToString(value);
 }
 inline std::ostream& operator << (std::ostream & os, const Vec3& value){
-    return os << Math::ToString(value);
+	return os << Math::ToString(value);
 }
 inline std::ostream& operator << (std::ostream & os, const Vec2& value){
-    return os << Math::ToString(value);
+	return os << Math::ToString(value);
 }
 inline std::ostream& operator << (std::ostream & os, const Quat& value){
-    return os << Math::ToString(value);
+	return os << Math::ToString(value);
 }
